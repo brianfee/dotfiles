e17438dd70f4f7bce5b6ede8bd9ebdd748fd3f38
Brian <brianfee7@gmail.com>
2019-12-21 16:55:55 -0700

    Modified lemonbar launch script to check screen width.

--- scripts/launch_bar.sh
+++ scripts/launch_bar.sh
@@ -4,7 +4,10 @@ killall -q lemonbar
 
 while pgrep -u $UID -x lemonbar > /dev/null;do sleep 1; done
 
+width=`xdpyinfo | awk '/dimensions:/ { print $2 }' | tr "x" "\n" | head -n 1`
+geom=$width"x64"
+
 eval $@ | lemonbar -p \
-	-g 3840x64 \
+	-g $geom \
 	-f "FontAwesome:size=12" \
 	-f "Noto Sans:style=bold:size=8.5" \
